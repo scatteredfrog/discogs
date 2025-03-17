9d411796c9dfe5cb3720ca74a02d680901103a72
Sean Courtney <scatteredfrog@users.noreply.github.com>
2025-03-17 17:40:52 -0500

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $field_id = 4; // or whatever the ID of your equivalent "Date washed" field is
 
 // The API call will not return all the items at once but in paginated pieces.
 // Here we set the call to pull the maximum number of pages allowed, to cut
-// down on the number of curl calls.
+// down on the number of cURL calls.
 $items_per_page = 100;
 
 // This is the error symbol that will appear in the header of the modal that
